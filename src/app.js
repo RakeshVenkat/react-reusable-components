f7b8c5f50229ae495e8af1e0aed2e19ed40cac33
Rakesh Venkat <raakx.eln@gmail.com>
2020-04-10 19:15:27 +1000

    removed unused links

--- src/app.js
+++ src/app.js
@@ -2,21 +2,15 @@ import React, { Fragment } from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 
-import Navigation from './components/Navigation'
 import Home from './routes/Home'
-import About from './routes/About'
-import Sample from './routes/Sample'
 import CustomHeader from './components/CustomHeader'
 
 
 export const App = () => (
   <Fragment>
     <Router history={createBrowserHistory()}>
-      <Navigation />
       <Switch>
         <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/sample" component={Sample}/>
         <Route path="/header" component={CustomHeader}/>
       </Switch>
     </Router>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,8 +5,6 @@ const Navigation = () => (
   <div>
     <ul>
       <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/sample">Sample</Link></li>
     </ul>
   </div>
 )
